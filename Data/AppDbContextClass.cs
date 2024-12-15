69f7dcdbda521800bd472371e8e3fcfe3effab86
John <johnpaulfrancis@gmail.com>
2024-12-15 19:38:02 +0530

    Created the Database Context

---
+++ Data/AppDbContextClass.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TurboNg_API.Models;
+
+namespace TurboNg_API.Data
+{
+    public class AppDbContextClass : DbContext
+    {
+        public AppDbContextClass(DbContextOptions<AppDbContextClass> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<UserEntity> UserMaster { get; set; }
+    }
+}
